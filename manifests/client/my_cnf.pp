e53e19a25b503d9e216d2df4fdce02f0302d189f
Jordi Clariana <jclariana@trovit.com>
2015-07-13 10:00:21 +0200

    Apply linter syntax changes

--- manifests/client/my_cnf.pp
+++ manifests/client/my_cnf.pp
@@ -1,31 +1,31 @@
 # Per user my.cnf configuration file
 define mysql::client::my_cnf (
-    $host                  = "localhost",
     $user,
-    $password               = "",
-    $system_user            = "root",
-    $socket                 = "/var/lib/mysql/run/mysqld.sock",
+    $host                  = 'localhost',
+    $password               = '',
+    $system_user            = 'root',
+    $socket                 = '/var/lib/mysql/run/mysqld.sock',
     $mysql_client_socket    = true,
     $mysql_client_port      = false,
     $mysql_client_have_ssl  = false,
     $mysql_client_include   = true,
-    $ssl_certs_path         = "",
-    $ssl_ca_filename        = "ca-cert.pem",
-    $ssl_cert_filename      = "client-cert.pem",
-    $ssl_key_filename       = "client-key.pem"
+    $ssl_certs_path         = '',
+    $ssl_ca_filename        = 'ca-cert.pem',
+    $ssl_cert_filename      = 'client-cert.pem',
+    $ssl_key_filename       = 'client-key.pem'
 ) {
 
     # define this here again cause we may invoke mysql::client
     # without having a server installed
-    $config_path = $operatingsystem ? {
-        "CentOS"  => "/etc",
-        default  => "/etc/mysql",
+    $config_path = $::operatingsystem ? {
+        'CentOS'  => '/etc',
+        default  => '/etc/mysql',
     }
 
     if ($mysql_client_have_ssl == true)
     {
-        if ($ssl_certs_path == "") {
-            if ($user == "root") {
+        if ($ssl_certs_path == '') {
+            if ($user == 'root') {
                 $ssl_certs_real_path = "/${user}/certs"
             } else {
                 $ssl_certs_real_path = "/home/${user}/certs"
@@ -38,50 +38,50 @@ define mysql::client::my_cnf (
         $mysql_client_ssl_cert  = "${ssl_certs_real_path}/${ssl_cert_filename}"
         $mysql_client_ssl_key   = "${ssl_certs_real_path}/${ssl_key_filename}"
 
-        $client_ssl_ca_priv_key    = extlookup("mysql_client_ssl_ca")
-        $client_ssl_cert_priv_key  = extlookup("mysql_client_ssl_cert")
-        $client_ssl_key_priv_key   = extlookup("mysql_client_ssl_key")
+        $client_ssl_ca_priv_key    = extlookup('mysql_client_ssl_ca')
+        $client_ssl_cert_priv_key  = extlookup('mysql_client_ssl_cert')
+        $client_ssl_key_priv_key   = extlookup('mysql_client_ssl_key')
 
-        file { "${ssl_certs_real_path}":
+        file { $ssl_certs_real_path:
             ensure  => directory,
-            owner   => "${user}",
-            mode    => 0644,
-            require => File["${title}"]
+            owner   => $user,
+            mode    => '0644',
+            require => File[$title]
         }
 
-        file { "${mysql_client_ssl_ca}":
+        file { $mysql_client_ssl_ca:
             ensure  => present,
-            content => "$client_ssl_ca_priv_key\n",
+            content => "${client_ssl_ca_priv_key}\n",
             mode    => '0644',
-            owner   => "${user}",
+            owner   => $user,
             group   => 'root',
-            require => File["${ssl_certs_real_path}"]
+            require => File[$ssl_certs_real_path]
         }
 
-        file { "${mysql_client_ssl_cert}":
+        file { $mysql_client_ssl_cert:
             ensure  => present,
-            content => "$client_ssl_cert_priv_key\n",
+            content => "${client_ssl_cert_priv_key}\n",
             mode    => '0644',
-            owner   => "${user}",
+            owner   => $user,
             group   => 'root',
-            require => File["${ssl_certs_real_path}"]
+            require => File[$ssl_certs_real_path]
         }
 
-        file { "${mysql_client_ssl_key}":
+        file { $mysql_client_ssl_key:
             ensure  => present,
-            content => "$client_ssl_key_priv_key\n",
+            content => "${client_ssl_key_priv_key}\n",
             mode    => '0644',
-            owner   => "${user}",
+            owner   => $user,
             group   => 'root',
-            require => File["${ssl_certs_real_path}"]
+            require => File[$ssl_certs_real_path]
         }
 
     }
 
-    file { "${title}":
-        owner   => "${system_user}",
-        group   => "root",
-        mode    => "0600",
-        content => template("mysql/client/my.cnf.erb"),
+    file { $title:
+        owner   => $system_user,
+        group   => 'root',
+        mode    => '0600',
+        content => template('mysql/client/my.cnf.erb'),
     }
 }
